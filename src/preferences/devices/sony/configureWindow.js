ab2dd53a41ff6ae911054a846ebbd00ca9ea0ea7
maniacx <macs18max@gmail.com>
2026-08-01 10:24:27 -0600

    eslint fixes

--- src/preferences/devices/sony/configureWindow.js
+++ src/preferences/devices/sony/configureWindow.js
@@ -486,7 +486,7 @@ export const ConfigureWindow = GObject.registerClass({
                 ];
 
                 this._autoPowerOffDropdown = new DropDownRowWidget({
-                title: _('Automatically Power Off When Not Worn'),
+                    title: _('Automatically Power Off When Not Worn'),
                     options: this._autoPowerOffLabels,
                     values: this._autoPowerOffValues,
                     initialValue: this._settingsItems['auto-power-time'],
